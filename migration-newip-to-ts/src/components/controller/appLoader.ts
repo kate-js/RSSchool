105226643b53a2dbe8be50ca736b460f00773638
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-07-01 14:31:36 +0200

    feat: add TS to loader, controller, appLoader files

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,6 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
+            //https://nodenews.herokuapp.com/'
             apiKey: '07abd18f5a264ee88a5d7fe05c1313ac', // получите свой ключ https://newsapi.org/
         });
     }
--- migration-newip-to-ts/src/components/controller/loader.ts
+++ migration-newip-to-ts/src/components/controller/loader.ts
@@ -1,11 +1,23 @@
+export interface IOptions {
+    options?: {
+        sources?: string;
+        apiKey?: string;
+        endpoint?: string;
+    };
+    endpoint: 'sources' | 'everything';
+}
+
 class Loader {
-    constructor(baseLink, options) {
+    private baseLink: string;
+    options: { apiKey: string };
+
+    constructor(baseLink: string, options: { apiKey: string }) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
+        { endpoint, options = {} }: IOptions,
         callback = () => {
             console.error('No callback for GET response');
         }
@@ -13,7 +25,7 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,18 +35,18 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    makeUrl(options: { sources?: string }, endpoint: string) {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
+            url += `${key}=${urlOptions[key as keyof typeof urlOptions]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    private load(method: string, endpoint: string, callback: (data?: string) => void, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
