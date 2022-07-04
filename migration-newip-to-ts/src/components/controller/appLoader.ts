7e9a9aeaf1d7bc8db169ba4a6e3baac294d5fdbd
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-07-04 15:15:44 +0200

    feat: change URL for fetch

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             //https://nodenews.herokuapp.com/'
             apiKey: '07abd18f5a264ee88a5d7fe05c1313ac', // получите свой ключ https://newsapi.org/
         });
