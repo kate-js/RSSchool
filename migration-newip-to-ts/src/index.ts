34ae424321acd4f93186bc3bb90560e8f684a6bf
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-07-04 21:51:37 +0200

    feat: add notification Nothing found

--- migration-newip-to-ts/src/index.ts
+++ migration-newip-to-ts/src/index.ts
@@ -11,16 +11,34 @@ searchList.addEventListener('input', (e: InputEventInit) => {
     element.trim();
     const listItems = document.querySelectorAll<HTMLElement>('.news-list div') as NodeListOf<HTMLElement>;
     if (element != '') {
+        let count = 0;
         listItems.forEach((elem: HTMLElement): void => {
             if (elem.innerText.search(element) == -1) {
                 elem.style.display = 'none';
+                count = count + 1;
             } else {
                 elem.style.display = 'block';
+                count = count - 1;
             }
+            console.log(count);
         });
+        if (count == 128) {
+            const notification = document.querySelector('.notification');
+            if (!notification) {
+                createElement();
+            }
+        }
     } else {
         listItems.forEach((elem) => {
             elem.style.display = 'block';
         });
     }
 });
+
+function createElement() {
+    const search = document.querySelector('.search') as HTMLElement;
+    const newDiv = document.createElement('div') as HTMLElement;
+    newDiv.classList.add('notification');
+    newDiv.innerHTML = 'Nothing found(';
+    search.append(newDiv);
+}
