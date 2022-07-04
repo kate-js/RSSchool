6d157b47edef9483acc5389a8960ab3078051b0c
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-07-04 15:21:35 +0200

    feat: add search, change style page

--- migration-newip-to-ts/src/index.ts
+++ migration-newip-to-ts/src/index.ts
@@ -1,5 +1,26 @@
 import App from './components/app/app';
 import './global.css';
 
-const app = new App();
+const app: App = new App();
 app.start();
+
+const searchList: Node = document.querySelector<HTMLElement>('#news-list') as Node;
+let element = '';
+searchList.addEventListener('input', (e: InputEventInit) => {
+    element = element + e.data;
+    element.trim();
+    const listItems = document.querySelectorAll<HTMLElement>('.news-list div') as NodeListOf<HTMLElement>;
+    if (element != '') {
+        listItems.forEach((elem: HTMLElement): void => {
+            if (elem.innerText.search(element) == -1) {
+                elem.style.display = 'none';
+            } else {
+                elem.style.display = 'block';
+            }
+        });
+    } else {
+        listItems.forEach((elem) => {
+            elem.style.display = 'block';
+        });
+    }
+});
